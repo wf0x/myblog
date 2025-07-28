95d6e226e3fc47c63e9382b12f9e186e247b97c6
wf0x <1049961711@qq.com>
2025-07-28 17:35:51 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,89 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto-gen-sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  themeConfig: {
+    logo: '/logo.png',
+    outlineTitle: '目录',
+    outline: [2,6],
+    // https://vitepress.dev/reference/default-theme-config
+       // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '大模型',
+        items:[
+          { text: '大模型',  link: '/docs/fontcont/大模型进阶之路' },
+          { text: '深度学习',  link: '/docs/fontcont/深度学习进阶之路' },
+        ]
+      },
+      {
+        'text': '建站历程', link: '/docs/vitepress'
+      }
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   },
+    //   {
+    //     text: 'Examples2',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    // sidebar: { 
+    //   "/": set_sidebar("/fontcont"),
+    // },
+
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025-present Evan You'
+    },
+  },
+  markdown: {
+    lineNumbers: true, // 开启代码块行号
+  },
+  mermaid: {
+    // refer https://mermaid.js.org/config/setup/modules/mermaidAPI.html#mermaidapi-configuration-defaults for options
+  },
+  mermaidPlugin: {
+    class: "mermaid my-class", // set additional css classes for parent container
+  },
+})
